bbef2401e1f73bda79b65701342e9c44f2a57920
MuhammadimYusoff <muhammadimyusoff@gmail.com>
2021-12-22 01:15:12 +0800

    refill content

---
+++ src/application/lockerInterface.java
@@ -0,0 +1,9 @@
+package application;
+
+public interface lockerInterface {
+    String getColor();
+    int getDoor();
+    int getAvalability();
+    int setAvailability();
+    String toString();
+}
---
+++ src/module-info.java
@@ -0,0 +1,5 @@
+module JavaAGroupProject {
+	requires javafx.controls;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
