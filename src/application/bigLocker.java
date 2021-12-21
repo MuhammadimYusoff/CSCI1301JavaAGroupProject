12b5380b1599d1583d2e4159ec4fd69a14624038
Ramadhani <ramadhani.nurfikri@gmail.com>
2021-12-22 02:25:39 +0800

    editing locker subclasses

--- src/application/bigLocker.java
+++ src/application/bigLocker.java
@@ -1,4 +1,17 @@
 package application;
 
-public class bigLocker {
+public class bigLocker extends locker {
+
+    private String size;
+    private int door;
+    private int shelves;
+
+    public String getSize(){
+        return size;
+    }
+
+    public void setSize(String size){
+        this.size = size;
+    }
+
 }
