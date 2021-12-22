2af004985e216702e1f12b8c5d219d8b43108b8f
Ramadhani <ramadhani.nurfikri@gmail.com>
2021-12-23 02:51:41 +0800

    deleting locker subclasses

--- src/application/lockers.java
+++ src/application/lockers.java
@@ -1,5 +1,23 @@
 package application;
 
+
+
 public class lockers {
 
+
+}
+
+class smallLocker extends locker {
+    smallLocker(int number, String colour, int door, char status, int availability) {
+    }
+}
+
+class mediumLocker extends locker {
+    mediumLocker(int number, String colour, int door, char status, int availability, int shelves) {
+    }
+}
+
+class largeLocker extends locker {
+    largeLocker(int number, String colour, int door, char status, int availability, int shelves) {
+    }
 }
