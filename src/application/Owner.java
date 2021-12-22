6e0adda4a73b3831a41a15560d91acdc726b8d73
Fadwa <fadram@gmail.com>
2021-12-22 17:38:20 +0300

    i know its not workin, ganna recheack it tonight

--- src/application/Owner.java
+++ src/application/Owner.java
@@ -1,8 +1,13 @@
 package application;
 
-import java.util.Date;
+import java.util.*;
 
-public class Owner {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Owner extends Application {
 
 	private String name;
 	private int id;
@@ -12,17 +17,17 @@ public class Owner {
 	private String duriation;
 	private double totalRent;
 
-	Owner() {
+	public Owner() {
 		date = new Date();
 	}
 
-	Owner(int id, int pin) {
+	public Owner(int id, int pin) {
 		date = new Date();
 		this.id = id;
 		this.pin = pin;
 	}
 
-	Owner(String name, int id, int phoneNo, int pin,
+	public Owner(String name, int id, int phoneNo, int pin,
 			String duriation, double totalRent) {
 		date = new Date();
 		this.name = name;
@@ -80,5 +85,54 @@ public class Owner {
 	public void setTotalRent(double totalRent) {
 		this.totalRent = totalRent;
 	}
+	
+	public void login( ) {
+		
+		Scanner input = new Scanner(System.in);
 
+		this.id=0000;
+		this.pin=0000;
+		
+		System.out.println("enter id and pin: ");
+		int inputId = input.nextInt();
+		int inputPin = input.nextInt();
+		
+		if(inputId == this.id && inputPin == this.pin)
+			System.out.println("login right");
+		else
+			System.out.println("wrong id or pin");
+		
+		///this just a trial i'll edit on that later 
+	}
+	
+
+	 /*  @Override
+	    public void start(Stage primaryStage) throws Exception{
+	        primaryStage.setResizable(false);
+	        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+	        primaryStage.setTitle("login interface");
+	        primaryStage.setScene(new Scene(root, 600, 400));
+	        primaryStage.show();
+	    }*/
+	
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	   
+	   public static void main(String[] args) {
+		launch(args);
+	}
+	
 }
+
+
+
+
